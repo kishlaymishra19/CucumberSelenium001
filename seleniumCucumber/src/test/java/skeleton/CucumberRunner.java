fdf27d0fff10d6bd40c340a934e9607b3bb46058
Kishlay Mishra <kishlaymshr19@gmail.com>
2019-02-28 16:31:40 +0530

    Added json file

--- seleniumCucumber/src/test/java/skeleton/CucumberRunner.java
+++ seleniumCucumber/src/test/java/skeleton/CucumberRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty","html:src/reports/html-report"},monochrome=true)
+@CucumberOptions(plugin={"pretty","html:src/reports/html-report","json:src/reports/report.json"},monochrome=true)
 public class CucumberRunner {
 
 }
--- seleniumCucumber/src/test/java/skeleton/LoginStepDef.java
+++ seleniumCucumber/src/test/java/skeleton/LoginStepDef.java
@@ -1,5 +1,6 @@
 package skeleton;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -9,7 +10,6 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
 import pages.LoginPage;
 
 public class LoginStepDef {
