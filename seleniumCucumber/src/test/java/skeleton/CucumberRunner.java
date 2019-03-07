ad81d7acc2ff5f7bbc04349a2dfc6ad7e2c7d4fe
Kishlay Mishra <kishlaymshr19@gmail.com>
2019-03-07 11:16:52 +0530

    Test

--- seleniumCucumber/src/test/java/skeleton/CucumberRunner.java
+++ seleniumCucumber/src/test/java/skeleton/CucumberRunner.java
@@ -8,5 +8,5 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin={"pretty","html:src/reports/html-report","json:src/reports/report.json"},monochrome=true)
 public class CucumberRunner {
-
+//Test
 }
